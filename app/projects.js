05335c97da9ee445290f2830fa3ccc5d3288197c
Michael Realy <mjrealy@gmail.com>
2017-07-15 10:01:32 -0500

    Added 2nd project

--- app/projects.js
+++ app/projects.js
@@ -9,12 +9,12 @@ var projects = [
             heroku: "not deployed"            
         },
         {
-            title: "tender",
-            short: "Recipe browsing app with the interface of a dating app.  Inspired by tinder, this app allows users to quickly browse through multiple recipes using the Seatgeek API.  They can set filters for what to search for, and browse multiple recipes side-by-side, and then route to the full recipe page when they've made a selection.",
-            goals: "Recipe browsing app with the interface of a dating app.  Inspired by tinder, this app allows users to quickly browse through multiple recipes using the Seatgeek API.  They can set filters for what to search for, and browse multiple recipes side-by-side, and then route to the full recipe page when they've made a selection.",
-            screenshots: ["/img/tender-1.PNG", "/img/tender-2.PNG", "/img/tender-3.PNG"],
-            tools: "HTML, CSS, Javascript, jQuery, Firebase, Spoonacular API",
-            github: "https://github.com/mrealy/tender",
+            title: "who ARE you?",
+            short: "Get to know you card game with safe for work and not safe work questions.  Ability to create new cards, delete cards, or edit them.",
+            goals: "Get to know you card game with safe for work and not safe work questions.  Ability to create new cards, delete cards, or edit them.",
+            screenshots: ["/img/whoAREyou-1.PNG", "/img/whoAREyou-2.PNG", "/img/whoAREyou-3.PNG"],
+            tools: "Node, express, handlebars, Sequelize, passport",
+            github: "https://github.com/thensle/whoAreYou_Game",
             heroku: "not deployed"            
         }
 ]
