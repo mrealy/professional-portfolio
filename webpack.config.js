e39225c141b23d99b85de1825c547903a2504339
Michael Realy <mjrealy@gmail.com>
2017-08-01 19:44:13 -0500

    webpack.config.js tweaked

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   // This is the entry point or start of our react applicaton
-  entry: "./app/app.js",
+  entry: "./app/app",
 
   // The plain compiled Javascript will be output into this file
   output: {
