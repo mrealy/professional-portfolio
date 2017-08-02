e2c9e7b1e6026d9267beda377357bdb321144619
Michael Realy <mjrealy@gmail.com>
2017-08-01 19:42:56 -0500

    added exclude to webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         test: /\.jsx?$/,
         // Webpack will only process files in our app folder. This avoids processing
         // node modules and server files unnecessarily
-        include: /app/,
+        exclude: /node_modules/,
         loader: "babel",
         query: {
           // These are the specific transformations we'll be using.
